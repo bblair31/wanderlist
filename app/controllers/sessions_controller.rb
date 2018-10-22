1bb89ab1a5a7a03300b32e65bd441488a9eaded2
TurtleFeeder <t.choong@yahoo.com>
2018-10-22 17:21:03 -0400

    built out login form & session create action method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,18 @@
 class SessionsController < ApplicationController
 
+  def new
+  end
+
   def create
-    @user = User.find_by(email: params[:email])
-    return head(:forbidden) unless @user.authenticate(params[:password])
-    session[:user_id] = @user.id
+    @user = User.find_by(email: params[:session][:email])
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash[:error] = "Invalid email/password."
+      redirect_to login_path
+    end
+
   end
 
 end ## End SessionsController
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   resources :reviews
   resources :countries, only: [:index, :show]
   resources :cities, only: [:index, :show]
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
