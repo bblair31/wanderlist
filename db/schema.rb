71b8387a363487f245548281ba2c057a6fcdd758
Brennan Blair <jbrennan.blair@gmail.com>
2018-10-22 13:49:51 -0400

    built out show page for city and country

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_22_160519) do
+ActiveRecord::Schema.define(version: 2018_10_22_172827) do
 
   create_table "cities", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,17 @@ ActiveRecord::Schema.define(version: 2018_10_22_160519) do
 
   create_table "countries", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "content"
+    t.date "date_visited"
+    t.date "review_date"
+    t.integer "user_id"
+    t.integer "city_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
