68dbd01bf4c88173fb25420f3188d1f731f943a3
Brennan Blair <jbrennan.blair@gmail.com>
2018-10-23 07:41:00 -0400

    update tour relationships across models

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,5 +1,6 @@
 class Country < ApplicationRecord
   has_many :cities, dependent: :destroy
   has_many :reviews, through: :cities
+  has_many :tours, through: :cities
   validates :name, presence: true, uniqueness: true
 end ### End of Country Class
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
   has_many :reviews
   has_many :cities, through: :reviews
+  has_many :tours, through: :cities
   has_secure_password
   validates :email, presence: true, uniqueness: true
 
   def full_name
-    "#{first_name} #{last_name}" 
+    "#{first_name} #{last_name}"
   end
 end
