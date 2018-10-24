b13400184b2d182dc3b8af41074d96ee28692452
Brennan Blair <jbrennan.blair@gmail.com>
2018-10-24 08:42:12 -0400

    Update login page, including routes and session user

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   resources :countries, only: [:index, :show]
   resources :cities, only: [:index, :show]
 
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
+  get    '/login',   to: 'sessions#new', as: "login"
+  post   '/login',   to: 'sessions#create', as: "sessions"
   delete '/logout',  to: 'sessions#destroy'
   get '/', to: 'sessions#welcome', as: 'welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
