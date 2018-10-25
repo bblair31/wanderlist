27b24cd6bf2524a74c04a8148f8743c9ee2f1fe0
Brennan Blair <jbrennan.blair@gmail.com>
2018-10-25 18:24:05 -0400

    corrected pending spec tests and add logo to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,6 @@ Rails.application.routes.draw do
   resources :conversations, only: [:index, :create] do
     resources :messages, only: [:index, :create]
   end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
