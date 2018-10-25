cf6e90cd4fa8dfb807fe0b2b02a6c01adc69fff6
Brennan Blair <jbrennan.blair@gmail.com>
2018-10-24 21:59:43 -0400

    small addition to conversation model for readablity

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -3,18 +3,22 @@ class Conversation < ApplicationRecord
   belongs_to :receiver, class_name: "User", foreign_key: "receiver_id"
   has_many :messages, dependent: :destroy
 
-  validates_uniqueness_of :sender_id, scope: :receiver_id
+  validates :sender_id, uniqueness: { scope: :receiver_id}
 
   scope :between, -> (sender_id,receiver_id) do
     where("(conversations.sender_id = ? AND conversations.receiver_id = ?) OR (conversations.receiver_id = ? AND conversations.sender_id = ?)", sender_id, receiver_id, sender_id, receiver_id)
     end
 
   def recipient(current_user)
-   self.sender_id == current_user.id ? self.receiver : self.sender
+   if self.sender_id == current_user.id
+     self.receiver
+   else
+     self.sender
+   end
   end
 
   def unread_message_count(current_user)
    self.messages.where("user_id != ? AND read = ?", current_user.id, false).count
   end
-  
+
 end ### End of Conversation Class
