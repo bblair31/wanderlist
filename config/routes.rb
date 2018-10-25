c384095515acf7b2f1b22b07b087183915a98768
Brennan Blair <jbrennan.blair@gmail.com>
2018-10-24 21:31:28 -0400

    built basic messaging feature with models and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'messages/index'
+  get 'conversations/index'
   resources :tours, only: [:index, :show]
   resources :users
   resources :reviews
@@ -12,6 +14,10 @@ Rails.application.routes.draw do
   get '/', to: 'sessions#welcome', as: 'welcome'
   get '/analytic', to: 'countries#analytic', as: 'analytic'
 
-    get '/search', to: 'users#index'
+  get '/search', to: 'users#index'
+
+  resources :conversations, only: [:index, :create] do
+    resources :messages, only: [:index, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
